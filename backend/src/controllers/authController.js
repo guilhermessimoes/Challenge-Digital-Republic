27a5ca39b186168ecf4561f1bf785f1ea0d6d3bf
Seu nome para exibição <guilhermejunios@gmail.com>
2022-02-07 21:05:39 -0300

    Ajustando o logout

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -75,11 +75,7 @@ const authController = {
         return res.status(200).json({ message: 'Login realizado com sucesso' });
     },
 
-    logout: async(req, res) =>{
-        if (!req.session.Usuario) {
-            return res.status(401).json({ message: 'Você não está logado.' });
-        }
-
+    logout: async(req, res) =>{  
         req.session.destroy();
 
         return res.status(201).json({ message: 'logout efetuado com sucesso.' });
